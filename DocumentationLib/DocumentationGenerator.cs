8c6984b17bdcbce14ebb3d877ea970273f1acd26
Nikolay Mihalev <122871592+nikolaymihalev@users.noreply.github.com>
2025-02-27 21:54:59 +0200

    Added static methods in Documentation generator

--- DocumentationLib/DocumentationGenerator.cs
+++ DocumentationLib/DocumentationGenerator.cs
@@ -1,5 +1,19 @@
-﻿namespace DocumentationLib;
+﻿using DocumentationLib.Common;
+using DocumentationLib.Enums;
+
+namespace DocumentationLib;
 
 public static class DocumentationGenerator
 {
+    public static string ExportModelAsYaml<T>() 
+        => ModelDocumentGenerator.GenerateModel<T>(DocumentType.Yaml);
+
+    public static string ExportModelAsJson<T>()
+        => ModelDocumentGenerator.GenerateModel<T>(DocumentType.Json);
+
+    public static string ExportModelAsMarkdown<T>()
+        => ModelDocumentGenerator.GenerateText<T>(DocumentType.Markdown);
+
+    public static string ExportModelAsCsv<T>()
+        => ModelDocumentGenerator.GenerateText<T>(DocumentType.Csv);
 }

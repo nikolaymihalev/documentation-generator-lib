8b59abb387a23e67c11ef828e1fecdffc20d4dec
Nikolay Mihalev <122871592+nikolaymihalev@users.noreply.github.com>
2025-03-04 21:15:07 +0200

    Create DocumentationAttribute.cs

---
+++ DocumentationLib/Attributes/DocumentationAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace DocumentationLib;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+public class DocumentationAttribute : Attribute
+{
+    public string Summary { get; set; }
+    public DocumentationAttribute(string summary) => Summary = summary;
+}

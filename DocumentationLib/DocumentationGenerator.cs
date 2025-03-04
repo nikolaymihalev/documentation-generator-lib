37b77757ff246043f46093716ea04c81e06c9ff2
Nikolay Mihalev <122871592+nikolaymihalev@users.noreply.github.com>
2025-03-04 21:23:08 +0200

    Added filter for T in methods

--- DocumentationLib/DocumentationGenerator.cs
+++ DocumentationLib/DocumentationGenerator.cs
@@ -5,15 +5,15 @@ namespace DocumentationLib;
 
 public static class DocumentationGenerator
 {
-    public static string ExportModelAsYaml<T>() 
+    public static string ExportModelAsYaml<T>() where T : class
         => ModelDocumentGenerator.GenerateModel<T>(DocumentType.Yaml);
 
-    public static string ExportModelAsJson<T>()
+    public static string ExportModelAsJson<T>() where T : class
         => ModelDocumentGenerator.GenerateModel<T>(DocumentType.Json);
 
-    public static string ExportModelAsMarkdown<T>()
+    public static string ExportModelAsMarkdown<T>() where T : class
         => ModelDocumentGenerator.GenerateText<T>(DocumentType.Markdown);
 
-    public static string ExportModelAsCsv<T>()
+    public static string ExportModelAsCsv<T>() where T : class
         => ModelDocumentGenerator.GenerateText<T>(DocumentType.Csv);
 }
